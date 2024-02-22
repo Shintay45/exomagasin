3d1f3e4f5d11555e7e8491b20a653690ea2ab7f4
b.deluca <b.deluca@GOS-VDI908>
2024-02-22 16:24:21 +0100

    Ajoutez des fichiers projet.

---
+++ exomagasincomplet/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Numerics;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace exomagasincomplet
+{
+    internal class Program
+         //Un magasin en ligne propose 3 articles
+         //       Coca : 1€ (stock 4)
+         //       Sandwich : 5€ (stock 2)
+         //       Porte : 200€ (stock 3)
+
+         //       Tant que l'utiliseur veut continuer ses achats, le programme
+         //       continue à lui proposer la liste des articles en stock
+         //       et lui demande de faire son choix(1 2 ou 3 )
+
+         //       Quand il choisit d'arrêter, le programme affiche le total à payer
+             
+         //       L'affichage des articles (stocké dans une collection) 
+         //       se fera à l'aide d'une méthode(void)
+
+         //       Le panier du client doit également être stocké dans une collection
+
+         //       le retrait d'un article du stock se fait via méthode
+
+         //       créer une méthode qui calcule le total des achats
+    {
+        
+        
+        static void Main(string[] args)
+        {
+
+            mesMethodes.initialiser();
+            while (mesMethodes.continuer)
+            {
+                mesMethodes.Affichage();
+            }
+
+            
+
+        }
+    }
+}
---
+++ exomagasincomplet/mesMethodes.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exomagasincomplet
+{
+    internal class mesMethodes
+    {
+        static Dictionary<string, int> stock = new Dictionary<string, int>();
+        static Dictionary<string, int> panier = new Dictionary<string, int>();
+        const int prixCoca = 1;
+        const int prixSandwish = 5;
+        const int prixPorte = 200;
+        internal static bool continuer = true;
+
+        internal static void initialiser()
+        {            
+            panier.Add("Coca-Cola", 0);
+            panier.Add("Sandwish", 0);
+            panier.Add("Porte", 0);
+            stock.Add("Coca-Cola", 4);
+            stock.Add("Sandwish", 2);
+            stock.Add("Porte", 3);
+        }
+        internal static void Affichage()
+        {
+            int counter = 1;
+            foreach (KeyValuePair<string, int> st in stock)
+            {
+                Console.WriteLine($"{counter} : {st.Key} - {st.Value}");
+                counter++;
+            }
+        Console.WriteLine("4 : quitter et faire le total des achats ");
+        Console.WriteLine("veuillez choisir un article : ");
+        int choixUtil = int.Parse(Console.ReadLine());
+        Acheter(choixUtil);
+        }
+
+
+        static void Acheter(int choix)
+        {
+            switch (choix)
+            {
+                case 1:
+                    stock["Coca-Cola"] -= 1;
+                    panier["Coca-Cola"] += 1;
+                    break;
+                case 2:
+                    stock["Sandwish"] -= 1;
+                    panier["Sandwish"] += 1;
+                    break;
+                case 3:
+                    stock["Porte"] -= 1;
+                    panier["Porte"] += 1;
+                    break;
+                case 4:
+                    addition();
+                    continuer = false;
+                    break;
+            }
+        }
+        static void addition()
+        {
+            int total = 0;
+            foreach (KeyValuePair<string, int> pa in panier)
+            {
+
+                if (pa.Key == "Coca-Cola") total = total + (prixCoca * pa.Value);
+                if (pa.Key == "Sandwish") total = total + (prixSandwish * pa.Value);
+                if (pa.Key == "Porte") total = total + (prixPorte * pa.Value);
+
+            }
+
+        Console.WriteLine($" vous devez payer {total} euro , merci pour votre achat et à la prochaine !");
+        }
+    }
+}
